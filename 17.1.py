b3d085a0858bc2639596d620b9a121bb6d19be23
elizaveta92 <elizaveta.ivanova92@inbox.ru>
2023-04-27 10:46:27 +0300

    Initial commit

---
+++ 17.1.py
@@ -0,0 +1,44 @@
+dog = 21
+cat = "liza girl"
+print(dog)
+print("22")
+print(dog, "22")
+print(dog, cat)
+age = 55
+print(25)
+print("Возрас:", age, "лет")
+age = age + 1
+print("Возрас:", age, "лет")
+print(dog + age)
+print (0.1+0.1*5-0.3*4)
+print ((3.14+0.3)/2+0.15)
+a = -13
+b = 7
+print (a, b)
+print (b)
+print (31%2+31%2)
+print (13%3*3-3**2)
+print (11*2.5/3)
+print (float(3.14159/2))
+numbers = input("1 2 3 4 5 6 7:")
+numbers_split = numbers.split()
+numbers_split = "\n".join(numbers_split)
+print(numbers_split)
+
+numbers = input('1 2 3')
+print (s[1])
+print (s[2])
+print (s[3])
+print = (numbers)
+
+
+
+
+
+
+
+
+
+
+
+
